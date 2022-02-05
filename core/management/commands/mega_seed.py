3a9f3b711fadd66f833d822a0f4635d77a86cc0c
ayaf-bless <blessambel1@gmail.com>
2022-02-05 03:25:28 +0300

    setting up restframwork

--- core/management/commands/mega_seed.py
+++ core/management/commands/mega_seed.py
@@ -1,18 +1,20 @@
 import random
 from datetime import datetime
+
 from django.core.management.base import BaseCommand
 from django_seed import Seed
-from users.models import User
+
 from rooms.models import Room, Photo
+from users.models import User
 
 
 class Command(BaseCommand):
-
     help = "It seeds the DB with tons of stuff"
 
     def handle(self, *args, **options):
         user_seeder = Seed.seeder()
-        user_seeder.add_entity(User, 20, {"is_staff": False, "is_superuser": False})
+        user_seeder.add_entity(User, 20,
+                               {"is_staff": False, "is_superuser": False})
         user_seeder.execute()
 
         users = User.objects.all()
