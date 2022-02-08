35fb5cd1b344411a76b9bf87b070160690b003b6
ayaf-bless <blessambel1@gmail.com>
2022-02-08 15:16:28 +0300

    getting bact t6o using viewset

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 from . import views
-from .viewsets import RoomViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r"", viewset=RoomViewSet, basename="rooms")
 
 app_name = "rooms"
 
-urlpatterns = router.urls
+urlpatterns = [path("", views.ListRoomsView.as_view()), path("<int:pk>/", views.SeeRoomView.as_view())]
