42c9e9045027054f23e17d259f272aa2173d8ef6
ayaf-bless <blessambel1@gmail.com>
2022-02-08 17:03:07 +0300

    room creation, part-2

--- rooms/views.py
+++ rooms/views.py
@@ -25,9 +25,13 @@ def room_view(request):
         serializer = ReadRoomSerializer(rooms, many=True).data
         return Response(serializer)
     elif request.method == "POST":
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
         serializer = WriteRoomSerializer(data=request.data)
         if serializer.is_valid():
-            return Response(status=status.HTTP_200_OK)
+            room = serializer.save(user=request.user)
+            room_serializer = ReadRoomSerializer(room).data
+            return Response(status=status.HTTP_200_OK, data=room_serializer)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
