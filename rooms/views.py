2eed8049403d2092db8177e308de3746d0456793
Bless Ambele <blessambel1@gmail.com>
2022-02-26 16:53:12 -0930

    including the search end point to the RoomView

--- rooms/views.py
+++ rooms/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .permissions import IsOwner
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import status, permissions
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -23,38 +23,39 @@ class RoomViewSet(ModelViewSet):
             permission_classes = [IsOwner]
         return [permission() for permission in permission_classes]
 
-@api_view(["GET"])
-def room_search(request):
-    max_price = request.GET.get("max_price", None)
-    min_price = request.GET.get("min_price", None)
-    beds = request.GET.get("beds", None)
-    bathrooms = request.GET.get("bathrooms", None)
-    bedrooms = request.GET.get("bedrooms", None)
-    lat = request.Get.get("lat", None)
-    lng = request.GET.get("lng", None)
-    filter_kwarg = {}
-    if max_price:
-        filter_kwarg["price__lte"] = max_price
-    if min_price:
-        filter_kwarg["price__gte"] = min_price
-    if beds:
-        filter_kwarg["beds__lte"] = beds
-    if bedrooms:
-        filter_kwarg["bedrooms__lte"] = bedrooms
-    if bathrooms:
-        filter_kwarg["bathrooms__lte"] = bathrooms
-    if lat and lng:
-        common = 0.005
-        filter_kwarg["lat__gte"] = float(lat) - common
-        filter_kwarg["lat__lte"] = float(lat) + common
-        filter_kwarg["lng__gte"] = float(lng) - common
-        filter_kwarg["lng__gte"] = float(lng) + common
-    paginator = PageNumberPagination()
-    paginator.page_size = 10
-    try:
-        rooms = Room.objects.filter(**filter_kwarg)
-    except ValueError:
-        rooms = Room.objects.prefetch_related()
-    results = paginator.paginate_queryset(rooms, request=request)
-    serializers = RoomSerializer(results, many=True)
-    return paginator.get_paginated_response(serializers.data)
+    @action(detail=False)
+    def search(self, request):
+        print(request)
+        max_price = request.GET.get("max_price", None)
+        min_price = request.GET.get("min_price", None)
+        beds = request.GET.get("beds", None)
+        bathrooms = request.GET.get("bathrooms", None)
+        bedrooms = request.GET.get("bedrooms", None)
+        lat = request.GET.get("lat", None)
+        lng = request.GET.get("lng", None)
+        filter_kwarg = {}
+        if max_price:
+            filter_kwarg["price__lte"] = max_price
+        if min_price:
+            filter_kwarg["price__gte"] = min_price
+        if beds:
+            filter_kwarg["beds__lte"] = beds
+        if bedrooms:
+            filter_kwarg["bedrooms__lte"] = bedrooms
+        if bathrooms:
+            filter_kwarg["bathrooms__lte"] = bathrooms
+        if lat and lng:
+            common = 0.005
+            filter_kwarg["lat__gte"] = float(lat) - common
+            filter_kwarg["lat__lte"] = float(lat) + common
+            filter_kwarg["lng__gte"] = float(lng) - common
+            filter_kwarg["lng__gte"] = float(lng) + common
+        paginator = self.paginator
+        paginator.page_size = 10
+        try:
+            rooms = Room.objects.filter(**filter_kwarg)
+        except ValueError:
+            rooms = Room.objects.prefetch_related()
+        results = paginator.paginate_queryset(rooms, request=request)
+        serializers = RoomSerializer(results, many=True)
+        return paginator.get_paginated_response(serializers.data)
